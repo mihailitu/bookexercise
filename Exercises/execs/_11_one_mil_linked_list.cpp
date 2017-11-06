101eaf9b6a2150fd700a018aa698060cc22b6186
Mihai Litu <litu_mihai@yahoo.com>
2017-11-06 15:21:14 +0200

    Problem 11. One million elem linked list

---
+++ Exercises/execs/_11_one_mil_linked_list.cpp
@@ -0,0 +1,9 @@
+#include "_11_one_mil_linked_list.h"
+
+/* How will you implement linked list with 1 million nodes? How will you access 999999 th node?
+ * Give some optimal design strategy and implementation.
+ */
+void _11_one_mil_linked_list()
+{
+
+}
---
+++ Exercises/execs/_11_one_mil_linked_list.h
@@ -0,0 +1,7 @@
+#ifndef _11_ONE_MIL_LINKED_LIST_H
+#define _11_ONE_MIL_LINKED_LIST_H
+
+// How will you implement linked list with 1 million nodes? How will you access 999999 th node? Give some optimal design strategy and implementation.
+void _11_one_mil_linked_list();
+
+#endif // _11_ONE_MIL_LINKED_LIST_H
