56d3d47e455313b931f37c92ee4fac9548c194d4
Mihai Litu <litu_mihai@yahoo.com>
2017-11-21 12:41:49 +0200

    Expression evaluator

---
+++ AlgorithmsAndDataStructures/include/expressions.h
@@ -0,0 +1,10 @@
+#ifndef EXPRESSIONS_H
+#define EXPRESSIONS_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stack>
+
+int result(std::string expr);
+#endif // EXPRESSIONS_H
---
+++ AlgorithmsAndDataStructures/src/unionfind.cpp
@@ -0,0 +1,6 @@
+#include "unionfind.h"
+
+UnionFind::UnionFind(int no) : N(no)
+{
+
+}
