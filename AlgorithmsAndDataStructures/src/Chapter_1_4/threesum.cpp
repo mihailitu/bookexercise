b7c79d36b6755b4cc42de753c2f169822a64cb2e
mihai <mihai.litu@google.com>
2019-10-30 13:26:29 +0200

    three sum

--- AlgorithmsAndDataStructures/src/Chapter_1_4/threesum.cpp
+++ AlgorithmsAndDataStructures/src/Chapter_1_4/threesum.cpp
@@ -17,6 +17,37 @@ long long count_brute_force(const std::vector<int> &a)
     return count;
 }
 
+bool binarySearch(const std::vector<int> &a, int val)
+{
+    unsigned long lo = 0;
+    unsigned long hi = a.size() - 1;
+
+    while(lo <= hi) {
+        unsigned long mid = lo + (hi - lo) / 2;
+        if(val < a[mid])
+            hi = mid - 1;
+        else if (val > a[mid])
+            lo = mid + 1;
+        else
+            return true;
+    }
+    return false;
+}
+
+// sort the data and do binary search for each a[i], a[j], do a binary search for -(a[i] + a[j])
+long long count_n_sq_log_n(std::vector<int> a)
+{
+    std::sort(a.begin(), a.end());
+    unsigned long N = a.size();
+
+    unsigned count = 0;
+    for(unsigned i = 0; i < N - 1; ++i)
+        for(unsigned j = i; j < N; ++j)
+            if (binarySearch(a, -(a[i] + a[j])))
+                ++count;
+    return count;
+}
+
 void readInts(const std::string &fName, std::vector<int> &elements)
 {
     std::ifstream testFile(fName);
@@ -52,4 +83,22 @@ TEST(Chapter_1_4, ThreeSum)
     }
 }
 
+TEST(Chapter_1_4, ThreeSumNsqLogN)
+{
+    std::vector<std::string> fileData = {
+        "data/1Kints.txt",
+        "data/2Kints.txt",
+        "data/4Kints.txt",
+        "data/8Kints.txt",
+        "data/16Kints.txt",
+        "data/32Kints.txt",
+        "data/1Mints.txt"
+    };
 
+    for(auto f : fileData) {
+        log_info("ThreeSumDeLuxe using: %s", f.c_str());
+        std::vector<int> data;
+        readInts(f, data);
+        log_info("%d  elements adds up to zero", count_n_sq_log_n(data));
+    }
+}
