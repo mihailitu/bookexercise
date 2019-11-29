638b64cb89d7026a2883baf9f237cd9aa89bf0a8
Mihai Litu <mihai.litu@gmail.com>
2019-11-29 13:57:11 +0200

    Add Knuth shuffle algorithm

--- AlgorithmsAndDataStructures/src/Chapter_2_1/shuffle.cpp
+++ AlgorithmsAndDataStructures/src/Chapter_2_1/shuffle.cpp
@@ -2,6 +2,8 @@
 #include <random>
 
 // assume data is sorted
+// O(2*N) complexity
+// O(3*N) space
 template<typename T>
 std::vector<T> shuffle_random(const std::vector<T> &data)
 {
@@ -17,9 +19,9 @@ std::vector<T> shuffle_random(const std::vector<T> &data)
         newIdxs.push_back(std::make_pair(dist(e2), i));
     }
 
-    auto pairCompaireSecond = [](const std::pair<int, double> &r1, const std::pair<int, double> &r2) {
-      return r1.second < r2.second;
-    };
+//    auto pairCompaireSecond = [](const std::pair<int, double> &r1, const std::pair<int, double> &r2) {
+//      return r1.second < r2.second;
+//    };
 
     //Sort based on random values - it will get us shuffled indexes
     std::sort(newIdxs.begin(), newIdxs.end());
@@ -30,6 +32,22 @@ std::vector<T> shuffle_random(const std::vector<T> &data)
     }
 
     return shuffled;
+}
+
+// O(N) time complexity
+// O(N) space
+template<typename T>
+void shuffle_knuth(std::vector<T> &data)
+{
+    std::random_device rd;
+    std::mt19937 e2(rd());
+    std::uniform_real_distribution<double> dist(0, 1);
+
+    unsigned len = data.size();
+    for(unsigned i = 0; i < len; ++i) {
+        unsigned r = static_cast<unsigned>(dist(e2) * (i + 1));
+        std::swap(data[r], data[i]);
+    }
 }
 
 TEST(Chapter_2_1, ShuffleRandom)
@@ -55,4 +73,37 @@ TEST(Chapter_2_1, ShuffleRandom)
 
     std::cout << "Equal elements after shuffle: " << equalElements << std::endl;
     ASSERT_TRUE(equalElements < N/10);
+}
+
+TEST(Chapter_2_1, ShuffleKnuth)
+{
+    unsigned N = 1000000;
+    std::vector<int> data;
+
+    for(unsigned i = 0; i < N; ++i)
+        data.push_back(static_cast<int>(i));
+
+    std::sort(data.begin(), data.end());
+
+    auto oldData = data;
+
+    shuffle_knuth(data);
+//    for(unsigned i = 0; i < N; ++i)
+//        std::cout << shuffled[i] << " ";
+//    std::cout << "\n";
+
+    unsigned equalElements = 0;
+    for(unsigned i = 0; i < N; ++i)
+        if(data[i] == oldData[i])
+            ++equalElements;
+
+    std::cout << "Equal elements after shuffle: " << equalElements << std::endl;
+    EXPECT_TRUE(equalElements < N/10);
+
+    std::sort(data.begin(), data.end());
+    for(unsigned i = 0; i < N; ++i)
+        if(data[i] != oldData[i]) {
+            EXPECT_TRUE(false) << "Elements should not differ: " << i << " [" << data[i] << ", " << oldData[i] << "]\n";
+        }
+
 }
