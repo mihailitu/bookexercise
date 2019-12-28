4241ccd2ce43dc14b9607178d891e10845ee9516
Mihai Litu <mihai.litu@gmail.com>
2019-12-28 10:21:04 +0200

    Start percolation

--- AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.cpp
+++ AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.cpp
@@ -1,6 +1,38 @@
 #include "percolation.h"
 
-Percolation::Percolation()
+Percolation::Percolation(unsigned _N) : N(_N)
 {
+    grid = std::vector<std::vector<bool>>(N, std::vector<bool>(N, false));
+//    for(unsigned i = 0; i < N; ++i) {
+//        grid.push_back(std::vector<bool>(N, false));
+//    }
+}
 
+// opens the site (row, col) if it is not open already
+void Percolation::open(unsigned row, unsigned col)
+{
+}
+
+// is the site (row, col) open?
+bool Percolation::isOpen(unsigned row, unsigned col)
+{
+    return false;
+}
+
+// is the site (row, col) full?
+bool Percolation::isFull(unsigned row, unsigned col)
+{
+    return false;
+}
+
+// returns the number of open sites
+unsigned Percolation::numberOfOpenSites()
+{
+    return 0;
+}
+
+// does the system percolate?
+bool Percolation::percolates()
+{
+    return false;
 }
--- AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.h
+++ AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.h
@@ -1,11 +1,30 @@
 #ifndef PERCOLATION_H
 #define PERCOLATION_H
 
+#include <vector>
 
 class Percolation
 {
+    unsigned N;
+    std::vector<std::vector<bool>> grid;
 public:
-    Percolation();
+    // creates n-by-n grid, with all sites initially blocked
+    Percolation(unsigned _N);
+
+    // opens the site (row, col) if it is not open already
+    void open(unsigned row, unsigned col);
+
+    // is the site (row, col) open?
+    bool isOpen(unsigned row, unsigned col);
+
+    // is the site (row, col) full?
+    bool isFull(unsigned row, unsigned col);
+
+    // returns the number of open sites
+    unsigned numberOfOpenSites();
+
+    // does the system percolate?
+    bool percolates();
 };
 
 #endif // PERCOLATION_H
