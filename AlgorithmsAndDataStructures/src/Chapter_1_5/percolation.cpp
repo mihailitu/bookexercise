32f73f7b7bdbf9d0c31bda66c190e1325d6cc95f
Mihai Litu <mihai.litu@gmail.com>
2019-12-28 11:14:09 +0200

    Add dummy test

--- AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.cpp
+++ AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.cpp
@@ -1,22 +1,24 @@
 #include "percolation.h"
+#include <iostream>
 
-Percolation::Percolation(unsigned _N) : N(_N)
+Percolation::Percolation(unsigned long _N) : N(_N), openSites(0)
 {
     grid = std::vector<std::vector<bool>>(N, std::vector<bool>(N, false));
-//    for(unsigned i = 0; i < N; ++i) {
-//        grid.push_back(std::vector<bool>(N, false));
-//    }
 }
 
 // opens the site (row, col) if it is not open already
 void Percolation::open(unsigned row, unsigned col)
 {
+    if (grid[row][col])
+        return;
+    grid[row][col] = true;
+    ++openSites;
 }
 
 // is the site (row, col) open?
-bool Percolation::isOpen(unsigned row, unsigned col)
+bool Percolation::isOpen(unsigned row, unsigned col) const
 {
-    return false;
+    return grid[row][col];
 }
 
 // is the site (row, col) full?
@@ -26,13 +28,22 @@ bool Percolation::isFull(unsigned row, unsigned col)
 }
 
 // returns the number of open sites
-unsigned Percolation::numberOfOpenSites()
+unsigned Percolation::numberOfOpenSites() const
 {
-    return 0;
+    return openSites;
 }
 
 // does the system percolate?
 bool Percolation::percolates()
 {
     return false;
+}
+
+void Percolation::print() const
+{
+    for(unsigned i = 0; i < N; ++i) {
+        for(unsigned j = 0; j < N; ++j)
+            std::cout << (grid[i][j] ? '0' : 'x');
+        std::cout << '\n';
+    }
 }
--- AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.h
+++ AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.h
@@ -5,26 +5,29 @@
 
 class Percolation
 {
-    unsigned N;
+    unsigned long N;
+    unsigned openSites;
     std::vector<std::vector<bool>> grid;
 public:
     // creates n-by-n grid, with all sites initially blocked
-    Percolation(unsigned _N);
+    Percolation(unsigned long _N);
 
     // opens the site (row, col) if it is not open already
     void open(unsigned row, unsigned col);
 
     // is the site (row, col) open?
-    bool isOpen(unsigned row, unsigned col);
+    bool isOpen(unsigned row, unsigned col) const;
 
     // is the site (row, col) full?
     bool isFull(unsigned row, unsigned col);
 
     // returns the number of open sites
-    unsigned numberOfOpenSites();
+    unsigned numberOfOpenSites() const;
 
     // does the system percolate?
     bool percolates();
+
+    void print() const;
 };
 
 #endif // PERCOLATION_H
