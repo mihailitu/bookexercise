935114aa12d06411ca8ef73c447c56a07686e8bd
Mihai Litu <mihai.litu@gmail.com>
2020-01-21 10:37:39 +0200

    BST

--- AlgorithmsAndDataStructures/src/Chapter_3_2/binarysearchtree.cpp
+++ AlgorithmsAndDataStructures/src/Chapter_3_2/binarysearchtree.cpp
@@ -12,6 +12,12 @@ class BinarySearchTree
         {}
     };
 
+    enum class ParseOrder {
+        PREORDER,
+        INORDER,
+        POSTORDER
+    };
+
     std::shared_ptr<Node> root = nullptr;
 
     std::shared_ptr<Node> insert(std::shared_ptr<Node> x, Key k, Value v) {
@@ -26,13 +32,23 @@ class BinarySearchTree
         return x;
     }
 
-    void _print(std::shared_ptr<Node> node) const {
+    void _print(std::shared_ptr<Node> node, ParseOrder order) const {
         if (node == nullptr)
             return;
 
-        _print(node->left);
-        _print(node->right);
-        std::cout << "[" << node->key << ", " << node->val << "]\n";
+        if (order == ParseOrder::PREORDER)
+            std::cout << "[" << node->key << ", " << node->val << "], ";
+
+        _print(node->left, order);
+
+        if (order == ParseOrder::INORDER)
+            std::cout << "[" << node->key << ", " << node->val << "], ";
+
+        _print(node->right, order);
+
+        if (order == ParseOrder::POSTORDER)
+            std::cout << "[" << node->key << ", " << node->val << "], ";
+
     }
 public:
     void add(Key k, Value v) {
@@ -50,26 +66,82 @@ public:
         }
         return nullptr;
     }
+
+    Value min() const {
+        std::shared_ptr<Node> x = root;
+        while(x != nullptr) {
+            if (x->left == nullptr)
+                return x->val;
+            x = x->left;
+        }
+        throw std::runtime_error("Empty BST");
+    }
+
+    Value max() const {
+        std::shared_ptr<Node> x = root;
+        while(x != nullptr) {
+            if (x->right == nullptr)
+                return x->val;
+            x = x->right;
+        }
+        throw std::runtime_error("Empty BST");
+    }
+
+    bool isEmpty() const {
+        return root == nullptr;
+    }
+
     void remove(Key k);
 
-    void print() const {
-        _print(root);
+    void print_inorder() const {
+        std::cout << "INORDER\n";
+        _print(root, ParseOrder::INORDER);
+        std::cout << "\n";
     }
+    void print_preorder() const {
+        std::cout << "PREORDER\n";
+        _print(root, ParseOrder::PREORDER);
+        std::cout << "\n";
+    }
+    void print_postorder() const {
+        std::cout << "POSTORDER\n";
+        _print(root, ParseOrder::POSTORDER);
+        std::cout << "\n";
+    }
+
 };
 
 TEST(Chapter_3_2, BinarySearchTrees)
 {
     std::vector<char> ascii;
-    for(unsigned i = 0; i < 255; ++i)
+    for(unsigned i = 97; i <= 122; ++i)
         ascii.push_back(static_cast<char>(i));
     std::random_shuffle(ascii.begin(), ascii.end());
+
     BinarySearchTree<unsigned char, int> bt;
-    for(unsigned char c : ascii)
+    for(unsigned char c : ascii) {
+        std::cout << "[" << c << ", " << static_cast<int>(c) << "]\n";
         bt.add(c, static_cast<int>(c));
-    bt.print();
+    }
+
+    bt.print_preorder();
+    bt.print_inorder();
+    bt.print_postorder();
+
+    std::cout << "Min: " << bt.min() << "\n";
+    std::cout << "Max: " << bt.max() << "\n";
 
     BinarySearchTree<unsigned char, int> bt1;
-    for(unsigned i = 0; i < 255; ++i)
-        bt1.add(static_cast<unsigned char>(i), i);
-    bt1.print();
+
+    try {
+        std::cout << "Min: " << bt1.min() << "\n";
+        std::cout << "Max: " << bt1.max() << "\n";
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+//    BinarySearchTree<unsigned char, int> bt1;
+//    for(unsigned i = 0; i < 256; ++i)
+//        bt1.add(static_cast<unsigned char>(i), i);
+//    bt1.print();
 }
