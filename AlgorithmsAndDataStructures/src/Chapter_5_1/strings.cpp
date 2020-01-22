640612249e5c7e3af86ab174ae1361bac83f9a29
Mihai Litu <mihai.litu@google.com>
2020-01-22 09:59:46 +0200

    Move on

---
+++ AlgorithmsAndDataStructures/src/Chapter_5_1/strings.cpp
@@ -0,0 +1,6 @@
+#include "strings.hpp"
+
+Strings::Strings()
+{
+
+}
---
+++ AlgorithmsAndDataStructures/src/Chapter_5_1/strings.hpp
@@ -0,0 +1,11 @@
+#ifndef STRINGS_HPP
+#define STRINGS_HPP
+
+
+class Strings
+{
+public:
+    Strings();
+};
+
+#endif // STRINGS_HPP
