41410ca5351e693478c6e45e2133d365f88a18b9
Mihai Litu <mihai.litu@gmail.com>
2020-03-03 15:17:58 +0200

    Re-arrange

---
+++ __old/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/main.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/main.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_2/1_2_1_Point2D.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_1_Point2D.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_2/1_2_2_interval1d.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_2_interval1d.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_2/1_2_6_circularstr.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_6_circularstr.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_2/1_2_7_recursive.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_7_recursive.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_2/1_2_9_binarysearch.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_9_binarysearch.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_3/bag_ex.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_3/bag_ex.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_3/expressions.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_3/expressions.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_4/exercises_1_4.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_4/exercises_1_4.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_4/threesum.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_4/threesum.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_4/twosum.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_4/twosum.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_5/percolation.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_5/percolation.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_5/testpercolation.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_5/testpercolation.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_5/testunionfind.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_5/testunionfind.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_5/unionfind.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_5/unionfind.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_1_5/unionfind_exec1.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_1_5/unionfind_exec1.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_1/convex_hull.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_1/convex_hull.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_1/elementarysorts.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_1/elementarysorts.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_1/shuffle.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_1/shuffle.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_2/mergesort.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_2/mergesort.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_3/quicksort.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_3/quicksort.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_4/heapsort.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_4/heapsort.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_2_4/maxpriorityqueue.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_2_4/maxpriorityqueue.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_3_1/symboltables.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_3_1/symboltables.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_3_2/binarysearchtree.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_3_2/binarysearchtree.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_3_3/balancedsearchtree.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_3_3/balancedsearchtree.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/Chapter_5_1/strings.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/Chapter_5_1/strings.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/euclid_gcd.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/euclid_gcd.cpp.o"
+  "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/src/util/util.cpp" "/home/mihai/Documents/work/bookexercise/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/src/util/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src/Chapter_5_1"
+  "../.."
+  "../../gtest"
+  "../../gtest/internal"
+  "../../gtest/internal/custom"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ __old/AlgorithmsAndDataStructures/__bin/CMakeFiles/segwick.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segwick.dir/main.cpp.o"
+  "CMakeFiles/segwick.dir/src/euclid_gcd.cpp.o"
+  "CMakeFiles/segwick.dir/src/util/util.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_1_Point2D.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_2_interval1d.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_6_circularstr.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_7_recursive.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_2/1_2_9_binarysearch.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_3/bag_ex.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_3/expressions.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_4/exercises_1_4.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_4/threesum.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_4/twosum.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_5/percolation.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_5/testpercolation.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_5/testunionfind.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_5/unionfind.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_1_5/unionfind_exec1.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_1/convex_hull.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_1/elementarysorts.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_1/shuffle.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_2/mergesort.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_3/quicksort.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_4/heapsort.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_2_4/maxpriorityqueue.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_3_1/symboltables.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_3_2/binarysearchtree.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_3_3/balancedsearchtree.cpp.o"
+  "CMakeFiles/segwick.dir/src/Chapter_5_1/strings.cpp.o"
+  "segwick.pdb"
+  "segwick"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segwick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
