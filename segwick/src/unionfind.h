d78f88178d1da331ddff1e17e22a41e26e455a4a
Mihai Litu <mihai.litu@gmail.com>
2020-04-07 12:23:05 +0300

    Add doc and todo's

--- segwick/src/unionfind.h
+++ segwick/src/unionfind.h
@@ -9,18 +9,31 @@ public:
     UnionFind(unsigned _n) : N(_n) {}
 
     // connect p and q
-    void connect(unsigned p, unsigned q);
+    void connect(unsigned p, unsigned q) {
+        // TODO: don't construct vector before hand, just insert up to p or q when connecting.
+        if(p >= N) {
+            increaseTo(p);
+            N = p;
+        }
+        if(q >= N) {
+            increaseTo(q);
+            N = q;
+        }
+    }
 
     // return true if p and q are connected, false otherwise
-    bool connected(unsigned p, unsigned q);
+    bool connected(unsigned p, unsigned q) {
+        return false;
+    }
 
     // return the number of elements
     unsigned capacity() const {
         return N;
     }
 private:
+    void increaseTo(T val); // TODO: implement!
     unsigned N; // the capacity
-    std::vector<std::vector<T>> elements;
+    std::vector<T> elements;
 };
 
 #endif // UNIONFIND_H
