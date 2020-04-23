c4b33f084e0c3e357a74e7d1187f62ad07e8f4e4
Mihai Litu <mihai.litu@gmail.com>
2020-04-23 07:56:32 +0300

    make

--- segwick/src/unionfind.h
+++ segwick/src/unionfind.h
@@ -3,45 +3,21 @@
 
 #include <vector>
 
+
 class QuickFind {
 public:
-    QuickFind(unsigned _n) : N(_n) {
-        rootID.resize(N);
-        for(unsigned i = 0; i < N; ++i)
-            rootID[i] = i; // each element is it's own root
-    }
+    QuickFind(unsigned _n);
 
     // connect p and q
-    void connect(unsigned p, unsigned q) {
-        // TODO: don't construct vector before hand, just insert up to p or q when connecting.
-        if(p >= N) {
-            // increaseTo(p);
-            N = p;
-        }
-        if(q >= N) {
-            // increaseTo(q);
-            N = q;
-        }
-
-        unsigned pID = rootID[p];
-        unsigned qID = rootID[q];
-
-        for(unsigned i = 0; i < N; ++i) // change all entries that have rootID[p] as root with rootID[q] as root
-            if (rootID[i] == pID)       // (at most 2N+2 array accesses)
-                rootID[i] = qID;
-    }
+    void connect(unsigned p, unsigned q);
 
     // return true if p and q are connected, false otherwise
-    bool connected(unsigned p, unsigned q) {
-        return rootID[p] == rootID[q];
-    }
+    bool connected(unsigned p, unsigned q);
 
     // return the number of elements
-    unsigned capacity() const {
-        return N;
-    }
+    unsigned capacity() const;
 private:
-    void increaseTo(unsigned val); // TODO: implement!
+    void increaseTo(unsigned val);
     unsigned N; // the capacity
     std::vector<unsigned> rootID;
 };
